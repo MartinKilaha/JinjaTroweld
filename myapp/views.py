c556b6455416641823ace3135eadf9dede32a32d
Martin Kilaha <martinkilaha@gmail.com>
2024-02-27 14:14:07 +0300

    initial commit

---
+++ myapp/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def service(request):
+    return render(request, 'service.html')
+
+
+def portfolio(request):
+    return render(request, 'portfolio.html')
